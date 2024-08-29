5db51259d01e26d4ad4e9ac784ca051842f0452c
DHANASANKAR2003 <146157469+DHANASANKAR2003@users.noreply.github.com>
2024-08-29 15:33:17 +0530

    Create  Wire Declaring.v

---
+++ HDLBITS/Basics/Wire Declaring.v
@@ -0,0 +1,16 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+	
+    wire int1,int2;
+    
+    assign int1 = a&b;
+    assign int2 = c&d;
+    assign out = int1|int2;
+    assign out_n = ~out;
+    
+endmodule
